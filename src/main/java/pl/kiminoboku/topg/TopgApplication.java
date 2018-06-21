24865a361831641dee9330a54c5d75b9e7ff8dbf
Radoslaw Lewandowski <kiminoboku@gmail.com>
2018-06-21 23:21:57 +0200

    Created spring boot project

---
+++ src/main/java/pl/kiminoboku/topg/TopgApplication.java
@@ -0,0 +1,12 @@
+package pl.kiminoboku.topg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopgApplication.class, args);
+	}
+}
